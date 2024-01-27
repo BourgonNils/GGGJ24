82a413b072a10c531020b745bd92f82d173330c2
BourgonNils <43369094+BourgonNils@users.noreply.github.com>
2024-01-27 15:09:22 +0100

    Add player prefab + placeholder Idle

--- UnityProject/Assets/Script/player.cs
+++ UnityProject/Assets/Script/player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.Animations;
 public class Player : MonoBehaviour
 {
     [SerializeField] [Range(1,6)] private int maxLife = 3;
@@ -9,12 +10,18 @@ public class Player : MonoBehaviour
     [Range(0, 1)] public int playerId = 0;
     public ColorBubble myColorBubble;
 
+    private Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
        if(myColorBubble != ColorBubble.ROUGE && myColorBubble != ColorBubble.BLEU)
             throw new System.Exception("Couleur de joueur non valide");
+        
+        myAnimator = GetComponent<Animator>();
+        if(myAnimator ==null)
+            throw new System.Exception("Le joueur a besoin d'un animator");
+
 
         resetLife();
     }
@@ -22,6 +29,7 @@ public class Player : MonoBehaviour
     public void resetLife()
     {
         life = maxLife;
+        myAnimator.SetInteger("HP", life);
     }
 
     public ColorBubble GetColorBubble() 
@@ -32,6 +40,8 @@ public class Player : MonoBehaviour
     {
         this.life--;
         Debug.Log("Player " + playerId + " laught, life remaining " + this.life);
+        myAnimator.SetInteger("HP", life);
+
         return this.life == 0;
     }
 
