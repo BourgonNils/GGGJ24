bc77348627a853e6e686bf959a462b52cffdc470
BourgonNils <43369094+BourgonNils@users.noreply.github.com>
2024-01-28 15:18:15 +0100

    Add victory screen

--- UnityProject/Assets/Script/Acceuil.cs
+++ UnityProject/Assets/Script/Acceuil.cs
@@ -48,7 +48,7 @@ public class Acceuil : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
 
 
-        GameManager.instance.startNewGame();
+        GameManager.instance.startNewGame(4f);
         Destroy(this.gameObject);
 
     }
--- UnityProject/Assets/Script/PanelGameOver.cs
+++ UnityProject/Assets/Script/PanelGameOver.cs
@@ -1,11 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelGameOver : MonoBehaviour
 {
+    public static PanelGameOver instance;
+
+    [SerializeField] Canvas myCanvas;
+
+    [SerializeField] GameObject panelVictoire1;
+    [SerializeField] GameObject panelVictoire2;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+
+
+    public void setVisible()
+    {
+        gameObject.SetActive(true);
+
+        if (GameManager.instance.playerOne.getLife() == 0)
+            panelVictoire2.SetActive(true);
+        else
+            panelVictoire1.SetActive(true);
+
+
+    }
+
+    public void onStartGame()
+    {
+        panelVictoire2.SetActive(false);
+        panelVictoire1.SetActive(false);
+        gameObject.SetActive(false);
+
+    }
+
+    public void onClickBtnRestart()
+    {
+        GameManager.instance.startNewGame(0f);
+    }
+
+
+
     private void Start()
     {
-        this.gameObject.SetActive(false);
+        onStartGame();
     }
 }
